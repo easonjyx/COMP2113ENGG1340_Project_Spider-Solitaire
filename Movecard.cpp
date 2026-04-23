aa56a2f43c07e342c6a0d816e9cda643245d8959
Doc477 <s2194038@fms2.edu.hk>
2026-04-23 13:51:01 +0800

    Update Movecard.cpp

--- Movecard.cpp
+++ Movecard.cpp
@@ -1,7 +1,15 @@
+#include "Movecard.h"
 #include "cards.h"
 #include <cstring>
+#include <iostream>
 using namespace std;
 bool Movecard(int initcol, int tarcol, int num, rec* head) {
-    if(!is
+    if(!isValidMove(initcol, tarcol, num, head)) {
+        std::cout << "Unvalid Move" << std::endl;
+        return false;
+    }
+    int initLast = GetLastIndex(initcol, head):
+    int moveStart = initLast
+
 void savehistory() {
 
