4d346b4160b3b1ea9de61c404cb8d8685bc9f954
Pan Ruifan <Jasonpan20070628@163.com>
2026-05-01 15:43:52 +0800

    Update DeckManager.h

--- DeckManager.h
+++ DeckManager.h
@@ -2,11 +2,15 @@
 #define DECKMANAGER_H
 #include "cards.h"
 
+extern int remainingDeck[104];
+extern int remainingCount;
+extern int completedSets;
 void initNewGame(int difficultySuits);
 //difficultySuits = 1, 2, 4
 void dealNextRow();
 int getRemainingCardCount();
 int getCompletedSets();
 void resetGame();
+void incrementCompletedSets(int delta);
 
 #endif
